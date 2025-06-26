96e40c47649b79cef0d811696cebc48dd61c5de0
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-06-26 16:18:16 +0200

    feat: add possible solution for Main

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,5 +1,22 @@
 package org.lessons.java.shop;
 
 public class Main {
+    public static void main(String[] args) {
+        Prodotto monopolyGame = new Prodotto("Monopoly", "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
+                16.00f,
+                20f);
 
+        monopolyGame.getPrice();
+        monopolyGame.getPriceWithIva();
+        monopolyGame.getExtendName();
+
+        Prodotto xboxController = new Prodotto("Xbox Controller",
+                "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.",
+                50f,
+                25f);
+
+        xboxController.getPrice();
+        xboxController.getPriceWithIva();
+        xboxController.getExtendName();
+    }
 }

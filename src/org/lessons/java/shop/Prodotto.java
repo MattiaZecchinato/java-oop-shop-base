63437ab54f8920da0e379462d9e8b13b2e12d067
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-06-26 16:17:59 +0200

    feat: add possible solution for Prodotto

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,42 @@
 package org.lessons.java.shop;
 
-public class Prodotto {
+import java.util.Random;
 
+public class Prodotto {
+    int code;
+    String name;
+    String description;
+    float price;
+    float iva;
+
+    Random randNum = new Random();
+
+    public Prodotto(String name, String description, float price, float iva) {
+        this.code = randNum.nextInt(100);
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.iva = iva;
+    }
+
+    public float getPrice() {
+        System.out.println(this.price);
+        return this.price;
+    }
+
+    public float getPriceWithIva() {
+        float calcIva = this.price * (this.iva / 100);
+        float finalPrice = this.price + calcIva;
+
+        System.out.println(finalPrice);
+        return finalPrice;
+    }
+
+    public String getExtendName() {
+
+        String extendedName = this.code + "-" + this.name;
+
+        System.out.println(extendedName);
+        return extendedName;
+    }
 }

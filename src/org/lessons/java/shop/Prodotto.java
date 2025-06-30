a8515a41a631370ccd16c29fb66ac43581cfd6f0
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-06-30 14:32:11 +0200

    feat: update attributes visibility and add new construtors

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -3,13 +3,21 @@ package org.lessons.java.shop;
 import java.util.Random;
 
 public class Prodotto {
-    int code;
-    String name;
-    String description;
-    float price;
-    float iva;
+    private int code;
+    private String name;
+    private String description;
+    private float price;
+    private float iva;
 
     Random randNum = new Random();
+
+    public Prodotto() {
+        this.code = randNum.nextInt(100);
+        this.name = "";
+        this.description = "";
+        this.price = 0f;
+        this.iva = 0f;
+    }
 
     public Prodotto(String name, String description, float price, float iva) {
         this.code = randNum.nextInt(100);
@@ -19,24 +27,57 @@ public class Prodotto {
         this.iva = iva;
     }
 
+    public int getCode() {
+        return this.code;
+    }
+
+    public void setName(String name) {
+        if (name != null) {
+            this.name = name;
+        }
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setDescription(String description) {
+        if (description != null) {
+            this.description = description;
+        }
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setPrice(float price) {
+        if (price >= 0) {
+            this.price = price;
+        }
+    }
+
     public float getPrice() {
-        System.out.println(this.price);
         return this.price;
+    }
+
+    public void setIva(float iva) {
+        if (iva >= 0) {
+            this.iva = iva;
+        }
+    }
+
+    public float getIva() {
+        return this.iva;
     }
 
     public float getPriceWithIva() {
         float calcIva = this.price * (this.iva / 100);
-        float finalPrice = this.price + calcIva;
 
-        System.out.println(finalPrice);
-        return finalPrice;
+        return this.price + calcIva;
     }
 
     public String getExtendName() {
-
-        String extendedName = this.code + "-" + this.name;
-
-        System.out.println(extendedName);
-        return extendedName;
+        return this.code + "-" + this.name;
     }
 }
